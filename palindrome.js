aa7a062703d476ec9d028349a66653297876c536
RHarsh0 <119388397+RHarsh0@users.noreply.github.com>
2022-11-30 13:56:43 +0530

    done correction

--- palindrome.js
+++ palindrome.js
@@ -1,11 +1,11 @@
 var str = "TinTin";
 var rev = "";
-N = atr.length;
+N = str.length;
    for(let i = N-1;i>=0;i--){
         
         rev += str[i]
     }
-    if(rev != str)
+    if(rev == str)
     console.log("Yes");
     else
     console.log("No");
